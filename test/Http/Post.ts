1bf2d22c35fb9cdcb1bd97dbe782a88096704ee6
TL <31960565+kredki@users.noreply.github.com>
2019-04-05 13:52:14 +0200

    add tests

--- test/Http/Post.ts
+++ test/Http/Post.ts
@@ -1,7 +1,61 @@
-import { HttpEvent, Context } from '../../handlers/types';
-import { postTemplate } from '../../handlers/Http/POST/post-handler';
-import { expect } from 'chai'; 
+import { HttpEvent, Context, Code } from '../../handlers/types';
+import { register } from '../../handlers/Http/POST/register-handler';
+import { search } from '../../handlers/Http/POST/search-handler';
+import { expect } from 'chai';
+import * as _ from 'lodash';
+import oasp4fn from '@oasp/oasp4fn';
 
-describe('postTemplate', () => {
-    it('Your post method tests should be here.');
+const EVENT = {
+    method: 'POST',
+    path: {},
+    body: {},
+    query: {},
+    headers: {}
+}
+
+let context: Context;
+
+let code: string;
+
+describe('register', () => {
+    it('The register should return an object, with the code and dateAndTime properties', (done: Function) => {
+        let event =  <HttpEvent>_.assign({}, EVENT, { body: { "name": "David", "email": "somenthing@something.com", "phone": "658974145"}});
+        register(event, context, (err: Error, res: Code) => {
+            try {
+                expect(err).to.be.null;
+                expect(res).to.be.an('object').that.contains.all.keys('code', 'dateAndTime');
+                code = res.code;
+                done();
+            }
+            catch(error){
+                done(error);
+            }
+        })
+    });
+});
+
+describe('search', () => {
+    it('The search should return an array with the items of the table Queue', (done: Function) => {
+        search(EVENT, context, (err: Error, res: object[]) => {
+            try {
+                expect(err).to.be.null;
+                expect(res).to.be.an('Array');
+                res.forEach(obj => {
+                    expect(obj).to.be.an('object');
+                    expect(obj).to.contain.all.keys(
+                        ['name', 'email', 'phone', 'code', 'dateAndTime']
+                    );
+                })
+                done();
+            }
+            catch(error){
+                done(error);
+            }
+        })
+    });
+});
+
+after(async () => {
+    if(code)
+        await oasp4fn.delete('Queue', code).promise();
 });
